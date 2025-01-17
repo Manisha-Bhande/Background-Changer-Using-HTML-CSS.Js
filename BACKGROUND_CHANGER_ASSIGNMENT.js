a73ede3d26bb988231134950c3f21b59fde59115
Manisha-Bhande <manishabhande2003@gmail.com>
2025-01-17 23:48:29 +0530

    Add files via upload

---
+++ BACKGROUND_CHANGER_ASSIGNMENT.js
@@ -0,0 +1,58 @@
+
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+    document.body.style.backgroundImage = ''; // Clear any background image
+}
+
+function changeBackgroundImage1(image) {
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower1.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+}
+function changeBackgroundImage2(image) {
+    
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower2.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+    
+}
+function changeBackgroundImage3(image) {
+   
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower3.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+    
+}
+function changeBackgroundImage4(image) {
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower4.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+}
+function changeBackgroundImage5(image) {
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower5.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+}
+function changeBackgroundImage6(image) {
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower6.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+}
+function changeBackgroundImage7(image) {
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower7.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+}
+function changeBackgroundImage8(image) {
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower8.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+}
+function changeBackgroundImage9(image) {
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower9.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+}
+function changeBackgroundImage10(image) {
+    document.body.style.backgroundImage = `url(file:///E:/online%20class/HTML%20CODES/flower12.jpeg)`;
+    document.body.style.backgroundColor = ''; // Clear any background color
+}
+function resetBackgroundColor() {
+    document.body.style.backgroundImage = '';
+    document.body.style.backgroundColor = '';
+}
+function resetBackground() {
+    document.body.style.backgroundImage = '';
+    document.body.style.backgroundColor = '';
+}
